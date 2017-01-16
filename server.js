3ce98f01ddd8a23014c302bc92f4dfe27e86eb83
Herman Ho <contact.hermanho@gmail.com>
2017-01-15 22:43:19 -0800

    prep application for heroku deployment

--- server.js
+++ server.js
@@ -3,9 +3,18 @@ var express = require('express');
 // Create app
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('Express server started on port 3000');
+app.listen(PORT, () => {
+  console.log('Express server started on port ' + PORT);
 });
